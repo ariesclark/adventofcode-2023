1c5ade66b87f37d4730c2847ed9fcb3ab3d3cf86
Aries Clark <me@ariesclark.com>
2022-12-01 00:25:05 -0700

    add readme files

--- src/day-1/index.ts
+++ src/day-1/index.ts
@@ -3,7 +3,6 @@ import path from "path";
 
 void (async () => {
 	const input = await readFile(path.resolve(__dirname, "input.txt"), "utf8");
-	console.log(input);
 
 	const value = input
 		.split("\r\n")
@@ -11,10 +10,12 @@ void (async () => {
 		.sort((a, b) => (a > b ? 1 : -1))
 		.reverse();
 
-	// Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+	// Find the Elf carrying the most Calories.
+	// How many total Calories is that Elf carrying?
 	console.log("part 1", value[0]);
 
-	// Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+	// Find the top three Elves carrying the most Calories.
+	//How many Calories are those Elves carrying in total?
 	console.log(
 		"part 2",
 		value.slice(0, 3).reduce((a, b) => a + b, 0)
